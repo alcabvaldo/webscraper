8d3623ecc6539b6e6a06464d87bbd5933911ecd3
alcabvaldo <jandronico@hotmail.com>
2022-05-16 16:55:15 -0400

    agregado rating y separado en funciones

--- webscraper/src/main/java/webscraper/webscraperTema1.java
+++ webscraper/src/main/java/webscraper/webscraperTema1.java
@@ -35,7 +35,7 @@ import org.jfree.data.category.DefaultCategoryDataset;
         //getters
         public String getNombre() { return nombre_lenguaje; }
         public Float getRating() {  return rating_github; }
-        public int getApariciones() {  return nro_apariciones; }
+        public Integer getApariciones() {  return nro_apariciones; }
 
         //setters
         public void setNombre(String nombre_lenguaje) {  this.nombre_lenguaje = nombre_lenguaje; }
@@ -44,29 +44,70 @@ import org.jfree.data.category.DefaultCategoryDataset;
     }
 
 public class webscraperTema1 {
-
-    public webscraperTema1() {
-    }
-    
-	//Arraylist<Tuple> arr=new Arraylist<Tuple>();
-	//Tuple t=new Tuple(double, int, int);
+	
+	//para el calculo de rating_github
+	public static int max_apariciones = 0;
+	public static int min_apariciones = 100000000;
 
 
     
 	public static void main(String[] args){
-		String link_github = "https://github.com/topics/";
                 
 		List<Language_data> tiobe_languages = new ArrayList<Language_data>();
-
 		cargar_nombres_en_Lista(tiobe_languages);
-                
-		//List<String> tiobe_language_names = get_lista_language_names();
-		//List<String> tiobe_language_numbers = new ArrayList<String>();
-		
-		
-		
 		
 		//conseguir el numero de apariciones de github
+		get_nro_apariciones(tiobe_languages);
+		
+		//guardar los datos en un txt
+        guardar_en_txt(tiobe_languages);
+		
+		//calcular el rating github
+		calcular_rating_github(tiobe_languages);
+                
+		// ordenar el array por rating
+		ordenar_por_rating(tiobe_languages);
+		
+		//imprimir en pantalla
+		
+		
+		//Gráfico ordenado por apariciones
+		ordenar_por_apariciones(tiobe_languages);
+		mostrar_ventana(tiobe_languages);
+	}
+	
+	public static void cargar_nombres_en_Lista(List<Language_data> tiobe_languages){
+
+		List<String> tiobe_language_names = get_lista_language_names();
+		for(String nombre : tiobe_language_names){
+			Language_data lenguage = new Language_data(nombre,null,0);
+			tiobe_languages.add(lenguage);
+		}       
+	}
+	
+	public static void ordenar_por_rating(List<Language_data> tiobe_languages){
+		//https://stackoverflow.com/questions/10396970
+		Collections.sort(tiobe_languages, new Comparator<Language_data>() {
+			@Override
+			public int compare(Language_data o1, Language_data o2) {
+				return o2.getRating().compareTo(o1.getRating());
+			}
+		});
+	}
+	
+	public static void ordenar_por_apariciones(List<Language_data> tiobe_languages){
+		//https://stackoverflow.com/questions/10396970
+		Collections.sort(tiobe_languages, new Comparator<Language_data>() {
+			@Override
+			public int compare(Language_data o1, Language_data o2) {
+				return o2.getApariciones().compareTo(o1.getApariciones());
+			}
+		});
+	}
+	
+	
+	public static void get_nro_apariciones(List<Language_data> tiobe_languages){
+		String link_github = "https://github.com/topics/";
 		for( int i=0;i<tiobe_languages.size();i++ ){
 			try{
 				String url = link_github+tiobe_languages.get(i).getNombre();
@@ -79,34 +120,16 @@ public class webscraperTema1 {
 				String str_nro_apariciones = titulo.text().split(" ")[2];
 				int nro_apariciones = Integer.parseInt(str_nro_apariciones.replaceAll(",", ""));
 				
+				//verifica si es un maximo o un minimo
+				if(max_apariciones<nro_apariciones) max_apariciones = nro_apariciones;
+				if(min_apariciones>nro_apariciones) min_apariciones = nro_apariciones;
+				
 				tiobe_languages.get(i).setApariciones(nro_apariciones);
 			}
 			catch(Exception e){
 				System.out.println(e.getMessage());
 			}
 		}
-		
-		//guardar los datos en un txt
-        guardar_en_txt(tiobe_languages);      
-                
-		// ordenar el array por rating
-		//https://stackoverflow.com/questions/10396970
-		/*Collections.sort(tiobe_languages, new Comparator<Language_data>() {
-			@Override
-			public int compare(Language_data o1, Language_data o2) {
-				return o2.getRating().compareTo(o1.getRating());
-			}
-		});
-*/
-		//imprimir en pantalla
-		//
-		//
-		//
-		//
-		
-		
-		//Gráfico
-		mostrar_ventana(tiobe_languages);
 	}
 	
 	//source: https://www.w3schools.com/java/java_files_create.asp
@@ -121,6 +144,14 @@ public class webscraperTema1 {
 		} catch (IOException e) {
 			System.out.println("An error occurred.");
 			e.printStackTrace();
+		}
+	}
+	
+	
+	public static void calcular_rating_github(List<Language_data> tiobe_languages){
+		for( int i=0;i<tiobe_languages.size();i++ ){
+			Float rating_github = (((float)tiobe_languages.get(i).getApariciones()-min_apariciones)/(max_apariciones-min_apariciones))*100;
+			tiobe_languages.get(i).setRating(rating_github);
 		}
 	}
 	
@@ -149,19 +180,9 @@ public class webscraperTema1 {
         frame.pack();
         frame.setVisible(true);
 	}
-	
-	public static void cargar_nombres_en_Lista(List<Language_data> tiobe_languages){
-
-		List<String> tiobe_language_names = get_lista_language_names();
-		for(String nombre : tiobe_language_names){
-			Language_data lenguage = new Language_data(nombre,null,0);
-			tiobe_languages.add(lenguage);
-		}       
-	}
         
 	public static List get_lista_language_names(){
 		List<String> tiobe_language_names = new ArrayList<String>();
-		tiobe_language_names.add("matlab");
 		tiobe_language_names.add("python");
 		tiobe_language_names.add("c");
 		tiobe_language_names.add("java");
@@ -181,6 +202,7 @@ public class webscraperTema1 {
 		tiobe_language_names.add("perl");
 		tiobe_language_names.add("lua");
 		tiobe_language_names.add("ruby");
+		tiobe_language_names.add("matlab");
 		*/
 		return(tiobe_language_names);
 	}
