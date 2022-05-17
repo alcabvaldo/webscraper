39ff46d64250b7e17d4b836d6b0102a02521ccd9
alcabvaldo <jandronico@hotmail.com>
2022-05-16 21:37:11 -0400

    cambios tema2

--- webscraper/src/main/java/webscraper/webscraperTema2.java
+++ webscraper/src/main/java/webscraper/webscraperTema2.java
@@ -1,15 +1,9 @@
 
-
-
-
 package webscraper;
 
-import java.text.SimpleDateFormat;
 import java.time.Duration;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.time.temporal.Temporal;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -30,16 +24,13 @@ import org.jfree.data.category.DefaultCategoryDataset;
     class tag_class{
         private String nombre_tag;
         private int nro_apariciones;
-
         public tag_class(String nombre_tag, int nro_apariciones) {
             this.nombre_tag = nombre_tag;
             this.nro_apariciones = nro_apariciones;
         }
-
         //getters
         public String getNombre() { return nombre_tag; }
         public Integer getApariciones() {  return nro_apariciones; }
-
         //setters
         public void setNombre(String nombre_lenguaje) {  this.nombre_tag = nombre_lenguaje; }
         public void setApariciones(int nro_apariciones) {  this.nro_apariciones = nro_apariciones; }
@@ -53,49 +44,15 @@ public class webscraperTema2 {
     }
     
 	public static void main(String[] args){
-        // se deja la url incompleta para agregarle despues el nro de pagina
-		String parametros_url = "?o=desc&s=updated&page=";
-		String link_github = "https://github.com/topics/"+tema_de_interes+parametros_url;
-		Map<String,Integer> tags_map = new HashMap<>();
-		List<tag_class> lista_tags = new ArrayList<tag_class>();
+		//Map<String,Integer> tags_map = new HashMap<>();
 		
 		//conseguir las apariciones de github
-                int numero=10;
-		for( int i=1;i<=numero;i++ ){
-                    try{
-                        String nro_pagina = Integer.toString(i);
-                        System.out.println("conectandose a: "+ link_github+nro_pagina );
-                        Document document = Jsoup.connect( link_github+nro_pagina ).get();
-						//selecciona articulos
-                        Elements lista_articulos = document.getElementsByClass("border rounded color-shadow-small color-bg-subtle my-4");
-                        
-						//por cada articulo, verifica la fecha, si es correcto guarda los tags
-                        for(Element articulo : lista_articulos){
-                            Elements elemento_fecha = articulo.select("relative-time");
-							String fecha_articulo = elemento_fecha.attr("datetime");
-                            //System.out.println("Fecha: "+fecha_articulo);
-                            if(actualizado_treinta_dias(fecha_articulo)){
-                                //System.out.println("Está actualizado");
-								String tag_class = "topic-tag topic-tag-link f6 mb-2";
-                                Elements lista_tags_elemento = articulo.getElementsByClass(tag_class);
-                                for(Element tag : lista_tags_elemento){
-									//aca se van a guardar las tags con un contador
-									//https://stackoverflow.com/questions/81346/
-									tags_map.merge(tag.text(), 1, Integer::sum);
-									//si no hay key coloca 1, si ya existe suma 1
-                                }
-                            }
-                        }
-                        
-                        //String pagina = document.text();//.split(" ")[2];
-                        //System.out.println("La pagina es: "+pagina);
-                    }
-                    catch(Exception e){
-                        System.out.println(e.getMessage());
-                    }
-		}
+        int cant_paginas_a_probar=15;
+		Map<String,Integer> tags_map = get_tags_relacionadas(cant_paginas_a_probar);
+		
 		
 		//cambio de hashmap a arraylist para poder ordenar
+		List<tag_class> lista_tags = new ArrayList<tag_class>();
 		for(String nombre: tags_map.keySet()){
 			tag_class tag = new tag_class(nombre,tags_map.get(nombre));
 			lista_tags.add(tag);
@@ -114,6 +71,46 @@ public class webscraperTema2 {
 		mostrar_ventana(lista_tags);
 	}
 	
+	
+	public static Map<String,Integer> get_tags_relacionadas(int cant_paginas_a_probar){
+        // se deja la url incompleta para agregarle despues el nro de pagina
+		Map<String,Integer> tags_map = new HashMap<>();
+		
+		String parametros_url = "?o=desc&s=updated&page=";
+		String link_github = "https://github.com/topics/"+tema_de_interes+parametros_url;
+		
+		for( int i=1;i<=cant_paginas_a_probar;i++ ){
+			try{
+				String nro_pagina = Integer.toString(i);
+				System.out.println("conectandose a: "+ link_github+nro_pagina );
+				Document document = Jsoup.connect( link_github+nro_pagina ).get();
+				//selecciona articulos
+				Elements lista_articulos = document.getElementsByClass("border rounded color-shadow-small color-bg-subtle my-4");
+
+				//por cada articulo, verifica la fecha, si es correcto guarda los tags
+				for(Element articulo : lista_articulos){
+					Elements elemento_fecha = articulo.select("relative-time");
+					String fecha_articulo = elemento_fecha.attr("datetime");
+					if(actualizado_treinta_dias(fecha_articulo)){
+						String tag_class = "topic-tag topic-tag-link f6 mb-2";
+						Elements lista_tags_elemento = articulo.getElementsByClass(tag_class);
+						for(Element tag : lista_tags_elemento){
+							//aca se van a guardar las tags con un contador
+							//https://stackoverflow.com/questions/81346/
+							tags_map.merge(tag.text(), 1, Integer::sum);
+							//si no hay key coloca 1, si ya existe suma 1
+						}
+					}
+				}
+			}
+			catch(Exception e){
+				System.out.println(e.getMessage());
+			}
+		}
+		return (tags_map);
+	}
+	
+	
 	public static void ordenar_por_apariciones(List<tag_class> lista_tags){
 		//https://stackoverflow.com/questions/10396970
 		Collections.sort(lista_tags, new Comparator<tag_class>() {
@@ -123,7 +120,6 @@ public class webscraperTema2 {
 			}
 		});
 	}
-	
 	
 	
 	public static void mostrar_ventana(List<tag_class> lista_tags){
@@ -154,6 +150,7 @@ public class webscraperTema2 {
         frame.setVisible(true);
 	}
 
+	
     //verifica si la fecha recibida es de hace menos de 30 dias
 	//https://stackoverflow.com/questions/20165564/calculating-days-between-two-dates-with-java
 	public static boolean actualizado_treinta_dias(String fecha_articulo_str){
